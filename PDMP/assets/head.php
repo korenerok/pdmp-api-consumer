bc0e222894bcb94b126ea440308146a12c21b60b
korenerok <marvin.bernal@gmx.com>
2023-09-18 18:53:44 -0400

    Added date fortmat to table

--- PDMP/assets/head.php
+++ PDMP/assets/head.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
+<meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/colreorder/1.7.0/css/colReorder.dataTables.min.css" rel="stylesheet">
   <link href="assets/css/styles.css" rel="stylesheet">
   <title>PDMP</title>
 </head>
--- PDMP/assets/script.php
+++ PDMP/assets/script.php
@@ -1,2 +1,35 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.js"></script>
+<script src="https://cdn.datatables.net/colreorder/1.7.0/js/dataTables.colReorder.min.js"></script>
+<script src="https://cdn.datatables.net/plug-ins/1.13.6/sorting/anti-the.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-<script src="assets/js/pagination.js"></script>
+
+
+<script>
+$(document).ready(function() {
+
+  var table = $('#table').DataTable( {
+    columnDefs:[{
+      targets:[2,3],
+      render: DataTable.render.datetime('MMM Do YYYY')
+    }],
+    order: [[2, 'desc']]
+  });
+
+
+
+  $('#table').on('click', 'td:first-child', function() {
+
+     var texto = $(this).text().trim();
+
+
+    var primeraPalabra = texto.split(' ')[0];
+
+
+    table.search(primeraPalabra).draw();
+
+  });
+});
+</script>
